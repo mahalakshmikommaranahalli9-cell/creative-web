653808b948b756ebf512175faceee12ffdefa607
KOMMARANAHALLI MAHALAKSHMI <mahalakshmikommaranahalli9@gmail.com>
2026-09-19 02:23:16 +0530

    Implement service worker for caching assets

--- game/sw.js
+++ game/sw.js
@@ -1,1 +1,67 @@
+const CACHE_NAME = "arrow-19-09-v1";
 
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./game.js",
+    "./levels.js",
+    "./manifest.json",
+    "./sw.js"
+];
+
+
+/* INSTALL */
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+
+/* ACTIVATE */
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys => {
+
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+
+        })
+    );
+
+    self.clients.claim();
+});
+
+
+/* FETCH */
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+
+            })
+
+    );
+});
